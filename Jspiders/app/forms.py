b7d0abf6d3d7eac2e12896bb01bf81d743d8f88b
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2024-01-09 18:10:11 +0530

    Add files via upload

--- Jspiders/app/forms.py
+++ Jspiders/app/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+
+def validate_for_a(data):
+  if data.lower().startswith('a'):
+    raise forms.ValidationError('Started with a')
 class  Jspiders(forms.Form):
-  Sname=forms.CharField()
+  Sname=forms.CharField(validators=[validate_for_a])
   Sid=forms.IntegerField()
   address=forms.CharField(widget=forms.Textarea)
+  email=forms.EmailField()
+  remail=forms.EmailField()
+
+  def clean(self):
+    e=self.cleaned_data['email']
+    r=self.cleaned_data['remail']
+    if e!=r:
+      raise forms.ValidationError('Emails not matched')
--- Jspiders/app/models.py
+++ Jspiders/app/models.py
@@ -5,3 +5,5 @@ class Pyspiders(models.Model):
   Sname=models.CharField(max_length=100)
   Sid=models.IntegerField()
   address=models.TextField()
+  email=models.EmailField(default='hi@gmail.com')
+  remail=models.EmailField(default='hi@gmail.com')
